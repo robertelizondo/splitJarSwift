c24fe6b7b6e05552fc5e8c2d04f59c3c32b30ca5
robertelizondo <robmelizondo@gmil.com>
2020-01-25 13:44:19 -0600

    initial commit 0.0

---
+++ splitJar/View Controllers /FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  splitJar
+//
+//  Created by Robert Elizondo on 1/25/20.
+//  Copyright © 2020 Robert Elizondo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ splitJar/View Controllers /SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  splitJar
+//
+//  Created by Robert Elizondo on 1/25/20.
+//  Copyright © 2020 Robert Elizondo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
